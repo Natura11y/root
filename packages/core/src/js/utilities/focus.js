f3c332f356856c76b460d236aafcb4b5e4c9d411
Carl Avidano <cavidano@gmail.com>
2026-09-11 04:07:06 -0400

    fix: correct modal scrolling and keyboard focus

--- packages/core/src/js/utilities/focus.js
+++ packages/core/src/js/utilities/focus.js
@@ -75,18 +75,15 @@ export const focusTrap = (element, firstFocusTarget = element) => {
                 const firstFocusableElement = focusableElements[0];
                 const lastFocusableElement = focusableElements[focusableElements.length - 1];
 
-                if (document.activeElement === lastFocusableElement) {
-                    if (!event.shiftKey) {
-                        event.preventDefault();
-                        firstFocusableElement?.focus();
-                    }
-                }
-
-                if (document.activeElement === firstFocusableElement) {
-                    if (event.shiftKey) {
-                        event.preventDefault();
-                        lastFocusableElement?.focus();
-                    }
+                if (
+                    event.shiftKey &&
+                    (document.activeElement === element || document.activeElement === firstFocusableElement)
+                ) {
+                    event.preventDefault();
+                    lastFocusableElement?.focus();
+                } else if (!event.shiftKey && document.activeElement === lastFocusableElement) {
+                    event.preventDefault();
+                    firstFocusableElement?.focus();
                 }
 
                 break;
